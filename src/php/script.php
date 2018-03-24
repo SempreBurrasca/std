056e74a431bf5307e230a6f8ab182c037d1d9b49
SempreBurrasca <gian.comandini@gmail.com>
2018-03-24 11:59:33 +0100

    added comments

--- src/php/script.php
+++ src/php/script.php
@@ -1,23 +1,22 @@
     <!-- particles.js container -->
     <div id="particles-js"></div>
-  
     <!-- stats - count particles -->
     <div class="count-particles">
         <span class="js-count-particles"></span> 
     </div>
+    <!--Importa librerie-->
     <script src='https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
     <script src='https://threejs.org/examples/js/libs/stats.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/svg.js/2.6.4/svg.min.js'></script>
     <script src="src/js/index.js"></script>
     <script src="src/js/anime.min.js"></script>
+    <!--Script Sottotitoli-->
     <script>
             /*Generazione sottotitoli*/
             setInterval(function(){ 
                                     var sezioni = ["DIGITAL STRATEGIES", "BLOCKCHAIN", "PRODUCTION"];
                                     /*var prova = document.getElementById("sottotitolo").innerHTML*/
                                     var randomSezioni = sezioni[Math.floor(Math.random()*sezioni.length)];
-                                    
-                                    
                                     document.getElementById("sottotitolo").innerHTML = randomSezioni;
                                     if (randomSezioni == "DIGITAL STRATEGIES") {
                                         document.getElementById("sottotitolo").style.letterSpacing = "29px";
@@ -30,7 +29,6 @@
                                     } else {
                                         document.getElementById("sottotitolo").style.letterSpacing = "72px";
                                         /*document.getElementById("sottotitolo").style.transition = "all 1s";*/
-
                                     };
                                     /*Prova Animazione
                                     var lineDrawing = anime({
@@ -42,9 +40,6 @@
                                         direction: 'alternate',
                                         loop: true
                                         });*/
-
-
-
                                 }, 3000);
             
 
